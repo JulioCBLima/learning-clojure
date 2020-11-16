cc2c9ef946e1486472ff7e2e03e52cbeeaadbe5b
Julio Lima <posytron@gmail.com>
2020-11-16 18:44:09 -0300

    Add aula2

--- alura_course/colecoes/src/colecoes/aula2.clj
+++ alura_course/colecoes/src/colecoes/aula2.clj
@@ -1,2 +1,13 @@
 (ns colecoes.aula2)
 
+(def vetor ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"])
+
+(defn conta
+  ([elementos]
+   (conta 0 elementos))
+  ([total-ate-agora elementos]
+   (if (seq elementos)
+    (recur (inc total-ate-agora) (next elementos))
+     total-ate-agora)))
+
+(conta vetor)
