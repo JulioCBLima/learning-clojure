fa6eff35e80ea0a9011704e454531fde337bd1a8
Julio Lima <posytron@gmail.com>
2021-02-09 17:00:56 -0300

    Aula 4

--- alura_course/mutatomref/src/mutatomref/aula3.clj
+++ alura_course/mutatomref/src/mutatomref/aula3.clj
@@ -25,3 +25,42 @@
     (pprint @hospital-silveira)))
 
 (testa-atomao)
+;; ----------
+
+(defn chega-em-malvado!
+  [hospital pessoa]
+  (swap! hospital h.logic/chega-em-pausado :espera pessoa)
+  (println "após inserir" pessoa))
+
+(defn simula-um-dia-em-paralelo
+  []
+
+  (let [hospital (atom (h.model/novo-hospital))]
+    (.start (Thread. (fn [] (chega-em-malvado! hospital "111"))))
+    (.start (Thread. (fn [] (chega-em-malvado! hospital "222"))))
+    (.start (Thread. (fn [] (chega-em-malvado! hospital "333"))))
+    (.start (Thread. (fn [] (chega-em-malvado! hospital "444"))))
+    (.start (Thread. (fn [] (chega-em-malvado! hospital "555"))))
+    (.start (Thread. (fn [] (chega-em-malvado! hospital "666"))))
+    (.start (Thread. (fn [] (Thread/sleep 8000) (pprint hospital))))))
+
+(simula-um-dia-em-paralelo)
+
+(defn chega-sem-malvado!
+  [hospital pessoa]
+  (swap! hospital h.logic/chega-em :espera pessoa)
+  (println "após inserir" pessoa))
+
+(defn simula-um-dia-em-paralelo-bonzinho
+  []
+
+  (let [hospital (atom (h.model/novo-hospital))]
+    (.start (Thread. (fn [] (chega-sem-malvado! hospital "111"))))
+    (.start (Thread. (fn [] (chega-sem-malvado! hospital "222"))))
+    (.start (Thread. (fn [] (chega-sem-malvado! hospital "333"))))
+    (.start (Thread. (fn [] (chega-sem-malvado! hospital "444"))))
+    (.start (Thread. (fn [] (chega-sem-malvado! hospital "555"))))
+    (.start (Thread. (fn [] (chega-sem-malvado! hospital "666"))))
+    (.start (Thread. (fn [] (Thread/sleep 1000) (pprint hospital))))))
+
+(simula-um-dia-em-paralelo-bonzinho)
