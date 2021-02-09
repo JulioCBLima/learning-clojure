74f389859fa91f332b9b003d37139f4be358ea0c
Julio Lima <posytron@gmail.com>
2021-02-09 12:27:30 -0300

    Aula 2

--- alura_course/mutatomref/src/mutatomref/aula1.clj
+++ alura_course/mutatomref/src/mutatomref/aula1.clj
@@ -1,6 +1,7 @@
 (ns mutatomref.aula1
   (:require [mutatomref.model :as h.model]
-            [mutatomref.logic :as h.logic]))
+            [mutatomref.logic :as h.logic])
+  (:use clojure.pprint))
 
 (defn simula-um-dia
   [])
@@ -56,4 +57,22 @@ hospital
 
 
 ;; --------------
+(defn chega-em-malvado
+  [pessoa]
+  (def hospital (h.logic/chega-em-pausado hospital :espera pessoa))
+  (println "após inserir" pessoa))
 
+(defn simula-um-dia-em-paralelo
+  []
+
+  (def hospital (h.model/novo-hospital))
+
+  (.start (Thread. (fn [] (chega-em-malvado "111"))))
+  (.start (Thread. (fn [] (chega-em-malvado "222"))))
+  (.start (Thread. (fn [] (chega-em-malvado "333"))))
+  (.start (Thread. (fn [] (chega-em-malvado "444"))))
+  (.start (Thread. (fn [] (chega-em-malvado "555"))))
+  (.start (Thread. (fn [] (chega-em-malvado "666"))))
+  (.start (Thread. (fn [] (Thread/sleep 4000) (pprint hospital)))))
+
+(simula-um-dia-em-paralelo)
