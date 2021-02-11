511d67fe2daf1aa5f8657bc87843ac655413fb57
Julio Lima <posytron@gmail.com>
2021-02-11 13:38:02 -0300

    Aula 2

--- alura_course/recprotomm/src/recprotmm/aula1.clj
+++ alura_course/recprotomm/src/recprotmm/aula1.clj
@@ -25,3 +25,69 @@
     ))
 
 (testa-uso-de-pacientes)
+
+(defrecord Paciente [id nome nascimento])
+
+(def p (->Paciente 15 "Guilherme" "18/9/1981"))
+p
+;; => {:id 15, :nome "Guilherme", :nascimento "18/9/1981"}
+(type p)
+;; => recprotmm.aula1.Paciente
+(record? p)
+;; => true
+
+(.nome p)
+;; => "Guilherme"
+
+(Paciente. 15 "Guilherme" "18/9/1981")
+;; => {:id 15, :nome "Guilherme", :nascimento "18/9/1981"}
+
+(defrecord PacienteV2 [^Long id nome nascimento])
+
+(map->Paciente {:id 15
+                :nome "Guilherme"
+                :nascimento "18/9/1981"})
+;; => {:id 15, :nome "Guilherme", :nascimento "18/9/1981"}
+
+(let [guilherme (->Paciente 15 "Guilherme" "18/9/1981")]
+  (println (:id guilherme)) ; => 15
+  (println (vals guilherme)) ; => (15 Guilherme 18/9/1981)
+  )
+
+(map->Paciente {:id 15
+                :nome "Guilherme"
+                :nascimento "18/9/1981"
+                :rg "222222"})
+;; => {:id 15, :nome "Guilherme", :nascimento "18/9/1981", :rg "222222"}
+
+(map->Paciente {:nome "Guilherme"
+                :nascimento "18/9/1981"
+                :rg "222222"})
+;; => {:id nil, :nome "Guilherme", :nascimento "18/9/1981", :rg "222222"}
+
+#_(->Paciente "Guilherme" "18/9/1981")
+;; => arity error
+
+(assoc (Paciente. nil "guilherme" "18/9/1981") :id 38)
+;; => {:id 38, :nome "Guilherme", :nascimento "18/9/1981"}
+
+(class (assoc (Paciente. nil "guilherme" "18/9/1981") :id 38))
+;; => recprotmm.aula1.Paciente
+
+(= 
+ (map->Paciente {:id 15
+                :nome "Guilherme"
+                :nascimento "18/9/1981"})
+ (map->Paciente {:id 15
+                :nome "Guilherme"
+                :nascimento "18/9/1981"}))
+;; => true
+
+(= 
+ (map->Paciente {:id 15
+                :nome "Guilherme"
+                :nascimento "18/9/1981"})
+ (map->Paciente {:id 18
+                :nome "Guilherme"
+                :nascimento "18/9/1981"}))
+;; => false
