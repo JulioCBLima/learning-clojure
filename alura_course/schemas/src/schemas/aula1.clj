fed2ace286dd867614c3912774a901943f309fef
Julio Lima <posytron@gmail.com>
2021-03-13 19:01:37 -0300

    Add schemas validation - Aula 1.2

--- alura_course/schemas/src/schemas/aula1.clj
+++ alura_course/schemas/src/schemas/aula1.clj
@@ -1,5 +1,6 @@
 (ns schemas.aula1
-  (:require [clojure.pprint :refer [pprint]]))
+  (:require [clojure.pprint :refer [pprint]]
+            [schema.core :as s]))
 
 (defn adiciona-paciente
   [pacientes paciente]
@@ -49,3 +50,53 @@
 ;; => nil
 ;; ^ o problema aqui é paciente tem vários schemas distintos (mapas, números, etc).
 ;;   nesse caso precisávamos usar o número, mas usamos o mapa inteiro
+
+;; -------------------------
+;; schemas
+
+(s/validate Long 15)
+;; => 15
+
+(s/validate Long "Guilherme") ;; => Execution Error...
+
+
+(s/defn teste-simples
+  [x :- Long]
+  x)
+
+(teste-simples 30)
+;; => 30
+
+(teste-simples "guilherme")
+;; => "guilherme"
+
+(s/set-fn-validation! true)
+
+(teste-simples 30)
+;; => 30
+
+(teste-simples "guilherme") ;; => Execution Error...
+
+;;-----------------
+;;definindo funcoes anteriores com schema
+
+(s/defn imprime-relatorio-de-paciente
+  [visitas 
+   paciente :- Long]
+  (get visitas paciente))
+
+(let [guilherme {:id 15
+                 :nome "Guilherme"}
+      daniela   {:id 20
+                 :nome "Daniela"}
+      paulo     {:id 25
+                 :nome "Paulo"}
+      pacientes (reduce adiciona-paciente {} [guilherme daniela paulo])
+      visitas   (-> {}
+                    (adiciona-visita 15 ["01/01/2019"])
+                    (adiciona-visita 20 ["01/02/2019" "01/01/2020"])
+                    (adiciona-visita 15 ["01/03/2019"]))]
+  (imprime-relatorio-de-paciente visitas guilherme))
+;; => ; Execution error (ExceptionInfo) at schemas.aula1/eval16310$imprime-relatorio-de-paciente (REPL:83).
+;; Input to imprime-relatorio-de-paciente does not match schema: 
+;; [nil (named (not (instance? java.lang.Long a-clojure.lang.PersistentArrayMap)) paciente)]
