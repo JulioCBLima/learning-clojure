4a6e1a2c08f819d47907edfc65dc24cdd2ce0003
Julio Lima <posytron@gmail.com>
2021-03-20 15:21:06 -0300

    Aula 3.1

--- alura_course/schemas/src/schemas/aula3.clj
+++ alura_course/schemas/src/schemas/aula3.clj
@@ -56,3 +56,66 @@
 ; Execution error (ExceptionInfo) at schemas.aula3/eval9012$novo-pedido (REPL:40).
 ; Input to novo-pedido does not match schema: 
 ;	   [nil (named (not (financeiro-maior-ou-igual-a-zero -12.98)) valor) nil]  
+
+;; ---------
+
+(def Numeros [s/Num])
+
+(s/validate Numeros [15])
+;; => [15]
+
+(s/validate Numeros [15 0 12])
+;; => [15 0 12]
+
+(s/validate Numeros [12 1.56 -1])
+;; => [12 1.56 -1]
+
+(s/validate Numeros [])
+;; => []
+
+(s/validate Numeros [nil])
+; Execution error (ExceptionInfo) at schema.core/validator$fn (core.clj:155).
+; Value does not match schema: [(not (instance? java.lang.Number nil))]
+
+(s/validate Numeros nil)
+;; => nil
+;; ^ nil é como uma sequencia vazia por isso é considerado um vetor de Numeros valido
+;;   isso vem do nil pulling
+
+(def Plano [s/Keyword])
+
+(def PacienteV2
+  {:id    PosInt
+   :nome  s/Str
+   :plano Plano})
+
+(s/validate PacienteV2 {:id 15
+                        :nome "Guilherme"
+                        :plano [:raio-x :ultrassom]})
+;; => {:id 15, :nome "Guilherme", :plano [:raio-x :ultrassom]}
+
+(s/validate PacienteV2 {:id 15
+                        :nome "Guilherme"
+                        :plano [:raio-x]})
+;; => {:id 15, :nome "Guilherme", :plano [:raio-x]}
+
+(s/validate PacienteV2 {:id 15
+                        :nome "Guilherme"
+                        :plano []})
+;; => {:id 15, :nome "Guilherme", :plano []}
+
+(s/validate PacienteV2 {:id 15
+                        :nome "Guilherme"
+                        :plano nil})
+;; => {:id 15, :nome "Guilherme", :plano nil}
+
+(s/validate PacienteV2 {:id 15
+                        :nome "Guilherme"
+                        :plano nil})
+
+(s/validate PacienteV2 {:id 15
+                        :nome "Guilherme"})
+; Execution error (ExceptionInfo) at schema.core/validator$fn (core.clj:155).
+; Value does not match schema: {:plano missing-required-key}
+; ^ gostaríamos que :plano fosse opcional
+
