585e3f8601c9e661b2caa411a49ceb69dbf8571f
Julio Lima <posytron@gmail.com>
2021-03-21 20:44:01 -0300

    Create test Alura course

---
+++ alura_course/test/src/test/test.clj
@@ -0,0 +1,7 @@
+(ns test.test
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ alura_course/test/test/test/test_test.clj
@@ -0,0 +1,7 @@
+(ns test.test-test
+  (:require [clojure.test :refer :all]
+            [test.test :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
