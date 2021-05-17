df3bf53975e8c6c538e50186073af795c23350ae
Julio Lima <posytron@gmail.com>
2021-05-17 18:47:41 -0300

    Aula 1.2

--- alura_course/generative/test/generative/logic_test.clj
+++ alura_course/generative/test/generative/logic_test.clj
@@ -9,20 +9,20 @@
   (testing "Que cabe numa fila vazia"
     (is (= true
            (logic/cabe-na-fila? {:espera []} :espera))))
+
+  (testing "Que cabe pessoas em filas de tamanho até 4 inclusive"
+    (doseq [fila (gen/sample (gen/vector gen/string-alphanumeric 0 4) 100)]
+      (is (logic/cabe-na-fila? {:espera fila} :espera))))
   
   (testing "Não cabe na fila quando a fila está cheia"
-    (is (= false
-           (logic/cabe-na-fila? {:espera [1 2 3 4 5]} :espera))))
+    (is (not (logic/cabe-na-fila? {:espera [1 2 3 4 5]} :espera))))
 
   (testing "Que não cabe na fila quando tem mais do que uma fila cheia"
-    (is (= false
-           (logic/cabe-na-fila? {:espera [1 2 3 4 5 6]} :espera))))
+    (is (not (logic/cabe-na-fila? {:espera [1 2 3 4 5 6]} :espera))))
 
   (testing "Que cabe na fila quando tem um pouco menos do que uma fila cheia"
-    (is (= true
-           (logic/cabe-na-fila? {:espera [1 2 3 4]} :espera)))
-    (is (= true
-           (logic/cabe-na-fila? {:espera [1 2]} :espera))))
+    (is (logic/cabe-na-fila? {:espera [1 2 3 4]} :espera))
+    (is (logic/cabe-na-fila? {:espera [1 2]} :espera)))
   
   (testing "Que ... quando o departamento não existe"
     (is (nil? (logic/cabe-na-fila? {:espera [1 2 3 4]} :raio-x)))))
