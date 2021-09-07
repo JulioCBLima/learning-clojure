354421d0da317bdf26850eb04795dd738ace37d0
Julio Lima <posytron@gmail.com>
2021-09-07 20:53:05 -0300

    cats: initial commit

---
+++ cats-playground/test/cats_playground/cats_playground_test.clj
@@ -0,0 +1,7 @@
+(ns cats-playground.cats-playground-test
+  (:require [clojure.test :refer :all]
+            [cats-playground.cats-playground :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
